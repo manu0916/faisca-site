b415dcfb952121af9331e8ae4c5f8b3d00e6786a
manu0916 <manu4432d@gmail.com>
2026-09-16 11:10:37 -0300

    Remove PWA e oferece apenas downloads EXE e APK

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 "use strict";
+// Retire the former browser installation when its owner returns to this site.
+async function retireBrowserInstallation() {
+  const legacyScope = new URL("app/", document.baseURI).href;
+  if ("serviceWorker" in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.filter(registration => registration.scope === legacyScope).map(registration => registration.unregister()));
+  }
+  if ("caches" in window) {
+    for (const name of await caches.keys()) {
+      if (!name.startsWith("faisca-app-")) continue;
+      const cache = await caches.open(name);
+      for (const request of await cache.keys()) {
+        if (request.url.startsWith(legacyScope)) await cache.delete(request);
+      }
+      if ((await cache.keys()).length === 0) await caches.delete(name);
+    }
+  }
+}
+retireBrowserInstallation().catch(() => {});
 document.documentElement.classList.add("js");
 const toggle = document.querySelector(".menu-toggle");
 const navigation = document.querySelector("#navegacao");
